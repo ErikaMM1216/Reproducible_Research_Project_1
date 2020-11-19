c75de08f609935e20b689184ff3e8333dd739ae7
ErikaMM1216 <69597007+ErikaMM1216@users.noreply.github.com>
2020-11-19 14:06:44 -0600

    Add files via upload

---
+++ 6_Imputing_missing_data.R
@@ -0,0 +1,24 @@
+if(!file.exists("getdata-projectfiles-UCI HAR Dataset.zip")) {
+  temp <- tempfile()
+  download.file("http://d396qusza40orc.cloudfront.net/repdata%2Fdata%2Factivity.zip",temp)
+  unzip(temp)
+  unlink(temp)
+}
+
+activity<-read.csv("activity.csv")
+
+missingIndex<-is.na(activity[,1])
+
+m<-mean(averageStepsbyInterval$steps)
+
+activity1<-activity
+activity1[missingIndex,1]<-m
+head(activity1)
+
+##     steps       date interval
+## 1 37.3826 2012-10-01        0
+## 2 37.3826 2012-10-01        5
+## 3 37.3826 2012-10-01       10
+## 4 37.3826 2012-10-01       15
+## 5 37.3826 2012-10-01       20
+## 6 37.3826 2012-10-01       25
