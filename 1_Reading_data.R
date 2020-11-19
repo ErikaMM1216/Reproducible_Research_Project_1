817dfa900e597e945a0df406b435028f20f92f57
ErikaMM1216 <69597007+ErikaMM1216@users.noreply.github.com>
2020-11-19 14:03:52 -0600

    Add files via upload

---
+++ 1_Reading_data.R
@@ -0,0 +1,9 @@
+if(!file.exists("getdata-projectfiles-UCI HAR Dataset.zip")) {
+  temp <- tempfile()
+  download.file("http://d396qusza40orc.cloudfront.net/repdata%2Fdata%2Factivity.zip",temp)
+  unzip(temp)
+  unlink(temp)
+}
+
+activity<-read.csv("activity.csv")
+head(activity)
